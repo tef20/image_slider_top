62b579c89664e5f3e1c663e5e369eacdecac0406
Chris Telford <christelford@hotmail.co.uk>
2021-10-20 13:07:27 +0100

    rotate elements function added

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
+const carousel = document.querySelector('.carousel');
+for (let i = 0; i < 5; i++) {
+  const imgContainer = document.createElement('div');
+  imgContainer.className = 'imgContainer';
+  carousel.appendChild(imgContainer);
+
+  const img = document.createElement('img');
+  img.src = '#';
+  img.alt = `image${i} placeholder`;
+
+  imgContainer.appendChild(img);
+}
+
 const backArrow = document.querySelector('.scrollButton.back');
 backArrow.addEventListener('click', (e) => {
-  shiftSlide(-1)
+  rotateChildElements(carousel, 1);  
 });
 
-const forwardArrow = document.querySelector('.scrollButton.back');
+const forwardArrow = document.querySelector('.scrollButton.forward');
 forwardArrow.addEventListener('click', (e) => {
-  shiftSlide(1)
+  rotateChildElements(carousel, -1);
 });
 
-function shiftSlide(n) {
-  document.querySelector('.inView');
-  
-  document.querySelectorAll('.imgContainer').forEach((imgContainer) => {
-    imgContainer.style.right = '200%';
-  });
+function rotateChildElements(parent, direction) {
+  if (parent.children.length > 1 && direction) {
+    const first = parent.firstElementChild;
+    const last = parent.lastElementChild;
+
+    if (direction > 0) {
+      const overflow = last;
+      parent.insertBefore(overflow, first);
+    } else if (direction < 0) {
+      const overflow = first;
+      parent.insertBefore(overflow, last.nextElementSibling);
+    }
+  }
 }
-// document.body.appendChild(newdiv);
